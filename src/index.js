e3d882b91970408f23be646e07259954a307636a
Jeremy Wood <js.wood206@gmail.com>
2021-03-04 22:11:05 -0800

    commit tree bfa9cb0da62e13fd92f4c58b5478c1966ab52595

--- src/index.js
+++ src/index.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyCollection = document.getElementById("toy-collection");
+fetch("http://localhost:3000/toys").then(resp => resp.json()).then(function(json) {
+for (let i = 0; i < json.length; i++) {
+  
+  const toyCard = document.createElement("div");
+  toyCard.classList.add("card");
+
+  const toyName = document.createElement("h2");
+  toyName.innerHTML = json[i].name;
+
+  const toyImg = document.createElement("img");
+  toyImg.classList.add("toy-avatar");
+  toyImg.setAttribute("src", json[i].image);
+
+  const toyLikes = document.createElement("p");
+  toyLikes.innerHTML = `${json[i].likes} Likes `;
+
+
+  const toyBtn = document.createElement("button");
+  toyBtn.classList.add("like-btn");
+  toyBtn.innerHTML = "Like <3"
+
+
+  toyCard.appendChild(toyName);
+  toyCard.appendChild(toyImg);
+  toyCard.appendChild(toyLikes);
+  toyCard.appendChild(toyBtn);
+  toyCollection.appendChild(toyCard);
+}
+console.log(json);
+})
